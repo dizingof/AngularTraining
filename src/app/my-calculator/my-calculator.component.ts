c728adc9c836680b26af2998f4136e0f2b8a835e
DESKTOP-KUSCP3I\roman <romannep1989@gmail.com>
2024-09-12 20:32:24 +0300

    add calc

--- src/app/my-calculator/my-calculator.component.ts
+++ src/app/my-calculator/my-calculator.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-calculator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './my-calculator.component.html',
   styleUrl: './my-calculator.component.scss'
 })
 export class MyCalculatorComponent {
+
+x: number = 0
+y: number = 0
+
+result: number = 0
+
+
+Add(x: number, y: number) :number{
+  return this.result = x + y;
+}
+
+ShowAdd() {
+  this.result = this.Add(this.x, this.y);
+  console.log(this.result)
+}
 
 }
